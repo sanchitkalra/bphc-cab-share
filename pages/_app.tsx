79acd5868e9015bd5c7087bf45888db27c84cbd1
Sanchit Kalra <sanchit.kalra1@gmail.com>
2023-01-05 13:30:31 +0530

    Init project auth tailwind, supabase, google-auth, nav

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import {
+  SessionContextProvider,
+  Session,
+  useUser
+} from '@supabase/auth-helpers-react'
+import { useState } from 'react'
+import Navbar from '../components/navbar'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps
+}: AppProps<{ initialSession: Session }>) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient())
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <>
+        <Navbar current="home" />
+        <Component {...pageProps} />
+      </>
+    </SessionContextProvider>
+  )
 }
